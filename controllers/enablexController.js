48995d7a019a42f79759e70f3e09de4459611ec1
Sreerag o s <sreeragsnair142@gmail.com>
2026-09-17 17:55:59 +0530

    update block screen

--- controllers/enablexController.js
+++ controllers/enablexController.js
@@ -54,8 +54,8 @@ const createRoom = async (req, res, next) => {
                 scheduled: false,
                 adhoc: true,
                 duration: 60,
-                moderators: '6',
-                participants: '6',
+                moderators: '1',
+                participants: '1',
                 quality: 'HD',
                 auto_recording: false,
                 screen_share: false
